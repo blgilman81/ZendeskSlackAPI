a980142a88a5127c94f728afcaa768a6cfa46ea1
roshambeaux <blgilman81@gmail.com>
2020-04-17 08:39:43 -0500

    updated front end

--- public/src/index.js
+++ public/src/index.js
@@ -1,18 +1,22 @@
 const button = document.querySelector('button');
-const endpoint = document.querySelector('#endpoint').innerHTML;
-const query1 = document.querySelector('#query1').innerHTML;
-const query2 = document.querySelector('#query2').innerHTML;
+const endpoint = document.querySelector('#endpoint');
+const query1 = document.querySelector('#query1');
+const query2 = document.querySelector('#query2');
 const result = document.querySelector('#result');
 
 button.addEventListener('click', e => {
-        const urlParam = buildQueryString(endpoint, query1, query2);
+        const p1 = endpoint.options[endpoint.selectedIndex].value;
+        const p2 = query1.options[query1.selectedIndex].value;
+        const p3 = query2.options[query2.selectedIndex].value;
+        console.log(p1, p2, p3);
+        const urlParam = buildQueryString(p1, p2, p3);
         fetch(`http://localhost:3000/test/search${urlParam}`)
                 .then(response => response.json())
                 .then(url => {
                         result.textContent = url;
                 })
                 .catch(error => {
-                        console.log(e);
+                        console.log(error);
                 });
 });
 
@@ -21,3 +25,8 @@ const buildQueryString = (endpoint, search1, search2) => {
         console.log(encodeURI(newString));
         return newString;
 };
+
+const getValue = sel =>
+        // console.log(sel.options[sel.selectedIndex].value);
+        sel.options[sel.selectedIndex].value;
+console.log(endpoint);
